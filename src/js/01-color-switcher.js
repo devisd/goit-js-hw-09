b83eec40a421eddcb292b4273cee3d4212f74f5d
devisd <89788799+devisd@users.noreply.github.com>
2022-03-29 23:16:41 +0700

    Task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,14 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+const bodyBackground = document.querySelector('body')
+
+startBtn.addEventListener('click', onChangeBackgroundColor)
+
+function onChangeBackgroundColor() {
+    return bodyBackground.style.backgroundColor = getRandomHexColor()
+}
+
