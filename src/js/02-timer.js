8eb2bfdfba86df76591c8e87aa671a5f272ace45
devisd <89788799+devisd@users.noreply.github.com>
2022-04-05 01:47:47 +0700

    1

---
+++ src/js/02-timer.js
@@ -0,0 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import flatpickr from "flatpickr";
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+  },
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+const inputDate = document.querySelector("#datetime-picker");
+flatpickr(inputDate, options);
+
+const startBtn = document.querySelector('[data-start]');
+startBtn.addEventListener('click', addLeadingZero)
+
+function addLeadingZero(value) {
+    console.log(value);
+}
+
