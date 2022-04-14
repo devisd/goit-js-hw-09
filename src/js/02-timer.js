8f5d3edbca3bbc19c5815bdb4214603d5385ca7e
devisd <89788799+devisd@users.noreply.github.com>
2022-04-14 21:30:28 +0700

    Task 2 Complete

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,15 +2,63 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css';
 
+const inputDate = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+
+const fieldFormSeconds = document.querySelector('.value[data-seconds]');
+const fieldFormMinutes = document.querySelector('.value[data-minutes]');
+const fieldFormHours = document.querySelector('.value[data-hours]');
+const fieldFormDays = document.querySelector('.value[data-days]');
+
+let inputChooseDate = null;
+
+startBtn.setAttribute('disabled', true);
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-      console.log(selectedDates[0]);
+  onClose(selectedDate) {
+    inputChooseDate = selectedDate[0].getTime();
+    let nowDate = options.defaultDate.getTime();
+    if (inputChooseDate < nowDate) {
+      Notify.failure('Please choose a date in the future');
+      return;
+    };
+
+    startBtn.removeAttribute('disabled');
+    startBtn.addEventListener('click', onBtnStart);
   },
 };
+
+flatpickr(inputDate, options);
+
+function onBtnStart() {
+  Notify.info('Start the countdown');
+  const timerId = setInterval(() => {
+    let result = inputChooseDate - Date.now();
+    if (result <= 1) {
+      clearInterval(timerId);
+      result = 0;
+      Notify.success('Selected date has arrived');
+    };
+
+    startBtn.setAttribute('disabled', true);
+    const timeRest = convertMs(result);
+
+    fieldFormSeconds.textContent = `${pad(timeRest.seconds)}`
+    fieldFormMinutes.textContent = `${pad(timeRest.minutes)}`
+    fieldFormHours.textContent = `${pad(timeRest.hours)}`
+    fieldFormDays.textContent = `${pad(timeRest.days)}`
+  }, 1000);
+};
+
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+};
+
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -29,15 +77,4 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
-
-const inputDate = document.querySelector("#datetime-picker");
-flatpickr(inputDate, options);
-
-const startBtn = document.querySelector('[data-start]');
-startBtn.addEventListener('click', addLeadingZero)
-
-function addLeadingZero(value) {
-    console.log(value);
-}
-
+};
