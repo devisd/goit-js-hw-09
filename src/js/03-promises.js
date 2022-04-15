deca0eeeca81c2922c44363a6e0f83adfc9053fc
devisd <89788799+devisd@users.noreply.github.com>
2022-04-15 12:05:42 +0700

    Task 3 Complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notify.failure(`Rejected promise ${position} in ${delay}ms`);
   }
+}
+
+const form = document.querySelector('form');
+const createBtn = document.querySelector('button');
+let DELAY = 0;
+let STEP = 0;
+let AMOUNT = 0;
+let amountValue = 0;
+
+form.elements.delay.addEventListener('input', onInput);
+form.elements.step.addEventListener('input', onStep);
+form.elements.amount.addEventListener('input', onAmount);
+createBtn.addEventListener('click', onCreatePromise);
+
+function onInput() {
+  DELAY = form.elements.delay.value;
+}
+function onStep() {
+  STEP = form.elements.step.value;
+}
+function onAmount() {
+  AMOUNT = form.elements.amount.value;
+}
+
+function onCreatePromise(e) {
+  e.preventDefault();
+
+  return new Promise(() => {
+    const interval = setInterval(() => {
+      amountValue += 1;
+      console.log(AMOUNT);
+      console.log(amountValue);
+
+      if (amountValue === Number(AMOUNT)) {
+        clearInterval(interval);
+      }
+      setTimeout(() => {
+      createPromise();
+    }, DELAY);
+    }, STEP);
+  })
 }
